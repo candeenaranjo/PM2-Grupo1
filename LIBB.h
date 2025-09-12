664061ecc180969a4f20190546e6b3ac53530a31
Candela Naranjo <candenaranjo05@gmail.com>
2025-09-12 16:03:21 -0300

    .....

--- LIBB.h
+++ LIBB.h
@@ -1,18 +1,33 @@
 #ifndef LIBB_H_INCLUDED
 #define LIBB_H_INCLUDED
-#include "Alumos.h"
+#include "Alumno.h"
 
-typedef struct Nodo{
-    Alumno alumno;
-    struct Nodo* siguiente;
-}Nodo;
+int LocalizarLIBT(char codigo[], Alumno LIBT[], int* pos, int cant, int* celdas){
+    (*pos)=0;
+    int li, ls, m;
+    li = 0;
+    ls = cant-1;
+    m = (li+ls+1)/2;
 
-typedef struct{
-    Nodo* direcciones[TAM];
-}LIBT;
+    while(li<ls && strcmp(LIBT[m]->codigo,codigo)!=0){
 
+        if(strcmp(LIBT[m]->codigo,codigo)<0){
+            li = m+1;
+        }else{
+            ls = m-1;
+        }
 
-int LocalizarLIBT()
+        m = (li+ls+1)/2;
+        (*celdas)++;
+    }
+    (*pos)= m;
+    if (strcmp(LIBT[m]->codigo,codigo)==0){
+        (*celdas)++;
+        return 0;               //localizacion exitosa
+    }else{
+        return 1;               //localizacion no exitosa
+    }
+}
 
 
 
