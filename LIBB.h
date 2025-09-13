28cde1d8892d51ea3f94e0d0373d8f531af6d84a
Candela Naranjo <candenaranjo05@gmail.com>
2025-09-13 20:55:24 -0300

    libt

--- LIBB.h
+++ LIBB.h
@@ -2,18 +2,24 @@
 #define LIBB_H_INCLUDED
 #include "Alumno.h"
 
-int LocalizarLIBT(char codigo[], Alumno LIBT[], int* pos, int cant, int* celdas){
+int LocalizarLIBT(char codigo[], Alumno *LIBT[], int* pos, int cant, int* celdas)
+{
     (*pos)=0;
+    (*celdas)=0;
     int li, ls, m;
     li = 0;
     ls = cant-1;
     m = (li+ls+1)/2;
 
-    while(li<ls && strcmp(LIBT[m]->codigo,codigo)!=0){
+    while(li<=ls && strcmp(LIBT[m]->codigo,codigo)!=0)
+    {
 
-        if(strcmp(LIBT[m]->codigo,codigo)<0){
+        if(strcmp(LIBT[m]->codigo,codigo)<0)
+        {
             li = m+1;
-        }else{
+        }
+        else
+        {
             ls = m-1;
         }
 
@@ -21,14 +27,97 @@ int LocalizarLIBT(char codigo[], Alumno LIBT[], int* pos, int cant, int* celdas)
         (*celdas)++;
     }
     (*pos)= m;
-    if (strcmp(LIBT[m]->codigo,codigo)==0){
+    if (li<=ls && strcmp(LIBT[m]->codigo,codigo)==0)
+    {
         (*celdas)++;
         return 0;               //localizacion exitosa
-    }else{
+    }
+    else
+    {
         return 1;               //localizacion no exitosa
     }
 }
 
+int AltaLIBT(Alumno aux, Alumno *LIBT[], int *cant, int *corrimientos)
+{
+    int pos,i;
+    int celdas = 0;
+    if((*cant)==TAM)
+    {
+        return 2;           //lista llena no se puede dar de alta
+    }
+    else
+    {
+        Alumno *aux1;
+        aux1 = (Alumno *)malloc(sizeof(Alumno));
+        if(aux1 == NULL)
+        {
+            return 3;       //fallo de asignacion de memoria
+        }
+        if(LocalizarLIBT(aux.codigo,LIBT,&pos,*cant,&celdas)==1 && pos <=(*cant))
+        {
+            for(i=(*cant); i>pos; i--)
+            {
+                LIBT[i]=LIBT[i-1];
+                (*corrimientos)=(*corrimientos)+0,5;
+            }
+
+            strcpy(aux1->codigo,aux.codigo);
+            strcpy(aux1->nombre,aux.nombre);
+            strcpy(aux1->correo,aux.correo);
+            aux1->nota = aux.nota;
+            strcpy(aux1->condicion,aux.condicion);
+
+            LIBT[pos]=aux1;
+            (*cant)++;
+            return 0;         //alta exitosa del alumno
+        }
+        else
+        {
+            return 1;       //no se pudo dar de alta porque el alumno ya estaba cargado
+        }
+    }
+}
+
+int BajaLIBT(Alumno aux, Alumno *LIBT[], int *cant, int *corrimientos){
+    int pos,i, celdas=0;
+    if(LocalizarLIBT(aux.codigo,LIBT,&pos,*cant,&celdas)==0 && LIBT[pos]->nota == aux.nota && strcmp(LIBT[pos]->codigo, aux.codigo)==0 && strcmp(LIBT[pos]->condicion, aux.condicion)==0 && strcmp(LIBT[pos]->correo, aux.correo)==0 && strcmp(LIBT[pos]->nombre, aux.nombre)==0)
+    {
+        Alumno *aBorrar = LIBT[pos];   // guardar puntero para liberar
+
+        for(i=pos; i<(*cant)-1;i++)     //consultar por el cant - 1
+            {
+                LIBT[i]=LIBT[i+1];
+                (*corrimientos)=(*corrimientos)+0,5;
+            }
+        (*cant)--;
+        free(aBorrar);
+        return 1;
+    }
+    else
+        return 0;
+}
+
+int EvocarLIBT(Alumno *aux,Alumno *LIBT[],char codigo[],int cant, int *celdas)
+{
+    int pos;
+
+    if(LocalizarLIBT(codigo,LIBT,&pos,cant,celdas)==0)
+    {
+        *aux = *LIBT[pos];           //evocacion exitosa
+        return 1;
+    }
+
+    return 0;                       //evocacion no exitosa
+}
+
+void LimpiarListaDePunteros(Alumno *Lista[], int *cantLista) {
+    for (int i = 0; i < *cantLista; i++) {
+        free(Lista[i]);             // libera la memoria asignada dinámicamente
+        Lista[i] = NULL;             // evita punteros colgados
+    }
+    *cantLista = 0;                 // reinicia el contador de elementos
+}
 
 
 #endif // LIBB_H_INCLUDED
--- main.c
+++ main.c
@@ -9,7 +9,7 @@ void gotoxy(int x, int y)
 }
 
 
-int compararEstructuras(Alumno LSO[], int *cantLSO)
+int compararEstructuras(Alumno LSO[], Alumno *LIBT[], int *cantLSO, int *cantLIBT)
 {
     int i, op=0;
     char codigo[8];
@@ -17,10 +17,19 @@ int compararEstructuras(Alumno LSO[], int *cantLSO)
     FILE* fp = fopen("Prueba.txt", "r");
 
     (*cantLSO)=0;
+    (*cantLIBT)=0;
 
+    LimpiarListaDePunteros(LIBT,cantLIBT);
+
+    //Variable costos LSO
     int altaLSO=0, bajaLSO=0, evocarLSO=0, maxaltaLSO=0, maxbajaLSO=0, maxevocarELSO=0, maxevocarFLSO=0;
     float sumaAltaLSO=0, sumaBajaLSO=0, sumaEvocarELSO=0, sumaEvocarFLSO=0;
     int cantAltaLSO=0, cantBajaLSO=0, cantEvocarELSO=0, cantEvocarFLSO=0;
+
+    //Variables costos LIBT
+    int altaLIBT=0, bajaLIBT=0, evocarLIBT=0, maxaltaLIBT=0, maxbajaLIBT=0, maxevocarELIBT=0, maxevocarFLIBT=0;
+    float sumaAltaLIBT=0, sumaBajaLIBT=0, sumaEvocarELIBT=0, sumaEvocarFLIBT=0;
+    int cantAltaLIBT=0, cantBajaLIBT=0, cantEvocarELIBT=0, cantEvocarFLIBT=0;
 
     if(fp==NULL)
     {
@@ -66,6 +75,7 @@ int compararEstructuras(Alumno LSO[], int *cantLSO)
 
                 if(op==1)                              //Alta
                 {
+                    //ALTA LSO
                     if(AltaLSO(aux,LSO,cantLSO,&altaLSO)==0)
                     {
 
@@ -79,12 +89,29 @@ int compararEstructuras(Alumno LSO[], int *cantLSO)
                         }
                         cantAltaLSO++;
                     }
-
                     //init
                     altaLSO = 0;
+
+                    //ALTA LIBT
+                    if(AltaLIBT(aux,LIBT,cantLIBT,&altaLIBT)==0)
+                    {
+
+                        //sumar para el promedio
+                        sumaAltaLIBT = sumaAltaLIBT + altaLIBT;
+
+                        //calcular el maximo
+                        if(altaLIBT>maxaltaLIBT)
+                        {
+                            maxaltaLIBT = altaLIBT;
+                        }
+                        cantAltaLIBT++;
+                    }
+                    //init
+                    altaLIBT = 0;
                 }
                 else                                    //Baja
                 {
+                    //BAJA LSO
                     if(BajaLSO(aux,LSO,cantLSO,&bajaLSO)==0)
                     {
 
@@ -98,170 +125,244 @@ int compararEstructuras(Alumno LSO[], int *cantLSO)
                         }
                         cantBajaLSO++;
                     }
-
+                    //init
                     bajaLSO = 0;
+
+                    //BAJA LIBT
+                    if(BajaLIBT(aux,LIBT,cantLIBT,&bajaLIBT)==0)
+                    {
+
+                        //sumar para el promedio
+                        sumaBajaLIBT = sumaBajaLIBT + bajaLIBT;
+
+                        //calcular el maximo
+                        if(bajaLIBT>maxbajaLIBT)
+                        {
+                            maxbajaLIBT = bajaLIBT;
+                        }
+                        cantBajaLIBT++;
+                    }
+
+                    bajaLIBT = 0;
                 }
+
             }
-            else
+
+        else
+        {
+            fscanf(fp, " %[^\n]", codigo);
+            for(int i = 0; codigo[i] != '\0'; i++)
+                codigo[i] = toupper(codigo[i]);
+
+            //EVOCAR LSO
+            if(EvocarLSO(&aux,LSO,codigo,*cantLSO, &evocarLSO)==0)      //Evocar Exitoso
             {
-                fscanf(fp, " %[^\n]", codigo);
-                for(int i = 0; codigo[i] != '\0'; i++)
-                    codigo[i] = toupper(codigo[i]);
 
-                if(EvocarLSO(&aux,LSO,codigo,*cantLSO, &evocarLSO)==0)      //Evocar Exitoso
+                //sumar para promedio
+                sumaEvocarELSO = sumaEvocarELSO + evocarLSO;
+
+                //calcular el maximo
+                if(evocarLSO>maxevocarELSO)
                 {
-
-                    //sumar para promedio
-                    sumaEvocarELSO = sumaEvocarELSO + evocarLSO;
-
-                    //calcular el maximo
-                    if(evocarLSO>maxevocarELSO)
-                    {
-                        maxevocarELSO = evocarLSO;
-                    }
-                    cantEvocarELSO++;
+                    maxevocarELSO = evocarLSO;
                 }
-                else                                                         //Evocar Fracaso
+                cantEvocarELSO++;
+            }
+            else                                                         //Evocar Fracaso
+            {
+
+                //sumar para el promedio
+                sumaEvocarFLSO = sumaEvocarFLSO + evocarLSO;
+
+                //calculucar el maximo
+                if(evocarLSO>maxevocarFLSO)
                 {
-
-                    //sumar para el promedio
-                    sumaEvocarFLSO = sumaEvocarFLSO + evocarLSO;
-
-                    //calculucar el maximo
-                    if(evocarLSO>maxevocarFLSO)
-                    {
-                        maxevocarFLSO = evocarLSO;
-                    }
-                    cantEvocarFLSO++;
+                    maxevocarFLSO = evocarLSO;
                 }
-
-                evocarLSO = 0;
-
+                cantEvocarFLSO++;
             }
 
-        }
-        fclose(fp);
+            evocarLSO = 0;
 
-        //LSO
-        printf("// Lista secuencial ordenada con busqueda binaria\n");
-        gotoxy(0,2);
+            //EVOCAR LIBT
+            if(EvocarLIBT(&aux,LIBT,codigo,*cantLIBT, &evocarLIBT)==0)      //Evocar Exitoso
+            {
 
-        //COSTOS MAXIMOS
-        printf("// Costos maximos ->");
-        gotoxy(25,2);
-        printf("Evocacion Exitosa= %d", maxevocarELSO);
-        gotoxy(55,2);
-        printf("Evocacion Fracaso= %d", maxevocarFLSO);
-        gotoxy(85,2);
-        printf("Alta= %d", maxaltaLSO);
-        gotoxy(100,2);
-        printf("Baja= %d", maxbajaLSO);
-        gotoxy(0,3);
+                //sumar para promedio
+                sumaEvocarELIBT = sumaEvocarELIBT + evocarLIBT;
 
-        //COSTOS MEDIOS
-        printf("// Costos Medios  ->");
-        gotoxy(25,3);
-        if(cantEvocarELSO!=0)
-        {
-            printf("Evocacion Exitosa= %.2f", sumaEvocarELSO/cantEvocarELSO);
-        }
-        else
-        {
-            printf("Evocacion Exitosa= 0");
-        }
-        gotoxy(55,3);
-        if(cantEvocarFLSO!=0)
-        {
-            printf("Evocacion Fracaso= %.2f", sumaEvocarFLSO/cantEvocarFLSO);
-        }
-        else
-        {
-            printf("Evocacion Fracaso= 0");
-        }
-        gotoxy(85,3);
-        if(cantAltaLSO!=0)
-        {
-            printf("Alta= %.2f", sumaAltaLSO/cantAltaLSO);
-        }
-        else
-        {
-            printf("Alta= 0");
-        }
-        gotoxy(100,3);
-        if(cantBajaLSO!=0)
-        {
-            printf("Baja= %.2f", sumaBajaLSO/cantBajaLSO);
-        }
-        else
-        {
-            printf("Baja= 0");
+                //calcular el maximo
+                if(evocarLIBT>maxevocarELIBT)
+                {
+                    maxevocarELIBT = evocarLIBT;
+                }
+                cantEvocarELIBT++;
+            }
+            else                                                         //Evocar Fracaso
+            {
+
+                //sumar para el promedio
+                sumaEvocarFLIBT = sumaEvocarFLIBT + evocarLIBT;
+
+                //calculucar el maximo
+                if(evocarLIBT>maxevocarFLIBT)
+                {
+                    maxevocarFLIBT = evocarLIBT;
+                }
+                cantEvocarFLIBT++;
+            }
+
+            evocarLIBT = 0;
         }
 
-        /*gotoxy(0,5);
-        printf("// Lista secuencial ordenada con busqueda secuencial");
-        gotoxy(0,6);
-        printf("// Costos maximos ->");
-        gotoxy(25,6);
-        printf("Evocacion Exitosa= %d", maxEvoLSE);
-        gotoxy(55,6);
-        printf("Evocacion Fracaso= %d", maxEvoLSF);
-        gotoxy(85,6);
-        printf("Alta= %d", maxAltaLS);
-        gotoxy(100,6);
-        printf("Baja= %d", maxBajaLS);
-        gotoxy(0,7);
-        printf("// Costos Medios  ->");
-        gotoxy(25,7);
-
-        if(cantEvoELSO!=0) printf("Evocacion Exitosa= %.2f", sumaEvoLSE/cantEvoELSO);
-        else printf("Evocacion Exitosa= 0");
-
-        gotoxy(55,7);
-        if(cantEvoFLSO) printf("Evocacion Fracaso= %.2f", sumaEvoLSF/cantEvoFLSO);
-        else printf("Evocacion Fracaso= 0");
-
-        gotoxy(85,7);
-        if(cantAltaLSO!=0) printf("Alta= %.2f", sumaAltaLS/cantAltaLSO);
-        else printf("Alta= 0");
-
-        gotoxy(100,7);
-        if(cantBajaLSO!=0) printf("Baja= %.2f", sumaBajaLS/cantBajaLSO);
-        else printf("Baja= 0");
-
-        gotoxy(0,9);
-        printf("// Arbol binario de busqueda");
-        gotoxy(0,10);
-        printf("// Costos maximos ->");
-        gotoxy(25,10);
-        printf("Evocacion Exitosa= %d", maxEvoABE);
-        gotoxy(55,10);
-        printf("Evocacion Fracaso= %d", maxEvoABF);
-        gotoxy(85,10);
-        printf("Alta= %.2f", maxAltaAB);
-        gotoxy(100,10);
-        printf("Baja= %.2f", maxBajaAB);
-        gotoxy(0,11);
-        printf("// Costos Medios  ->");
-        gotoxy(25,11);
-
-        if(cantEvoEABB!=0) printf("Evocacion Exitosa= %.2f", sumaEvoABE/cantEvoEABB);
-        else printf("Evocacion Exitosa= 0");
-
-        gotoxy(55,11);
-        if(cantEvoFABB!=0) printf("Evocacion Fracaso= %.2f", sumaEvoABF/cantEvoFABB);
-        else printf("Evocacion Fracaso= 0");
-
-        gotoxy(85,11);
-        if(cantAltaABB!=0) printf("Alta= %.2f", sumaAltaAB/cantAltaABB);
-        else printf("Alta= 0");
-
-        gotoxy(100,11);
-        if(cantBajaABB!=0) printf("Baja= %.2f", sumaBajaAB/cantBajaABB);
-        else printf("Baja= 0");*/
-
-        gotoxy(0,13);
     }
-    return 1;
+    fclose(fp);
+
+    //LSO
+    printf("// Lista secuencial ordenada con busqueda binaria\n");
+    gotoxy(0,2);
+
+    //COSTOS MAXIMOS
+    printf("// Costos maximos ->");
+    gotoxy(25,2);
+    printf("Evocacion Exitosa= %d", maxevocarELSO);
+    gotoxy(55,2);
+    printf("Evocacion Fracaso= %d", maxevocarFLSO);
+    gotoxy(85,2);
+    printf("Alta= %d", maxaltaLSO);
+    gotoxy(100,2);
+    printf("Baja= %d", maxbajaLSO);
+    gotoxy(0,3);
+
+    //COSTOS MEDIOS
+    printf("// Costos Medios  ->");
+    gotoxy(25,3);
+    if(cantEvocarELSO!=0)
+    {
+        printf("Evocacion Exitosa= %.2f", sumaEvocarELSO/cantEvocarELSO);
+    }
+    else
+    {
+        printf("Evocacion Exitosa= 0");
+    }
+    gotoxy(55,3);
+    if(cantEvocarFLSO!=0)
+    {
+        printf("Evocacion Fracaso= %.2f", sumaEvocarFLSO/cantEvocarFLSO);
+    }
+    else
+    {
+        printf("Evocacion Fracaso= 0");
+    }
+    gotoxy(85,3);
+    if(cantAltaLSO!=0)
+    {
+        printf("Alta= %.2f", sumaAltaLSO/cantAltaLSO);
+    }
+    else
+    {
+        printf("Alta= 0");
+    }
+    gotoxy(100,3);
+    if(cantBajaLSO!=0)
+    {
+        printf("Baja= %.2f", sumaBajaLSO/cantBajaLSO);
+    }
+    else
+    {
+        printf("Baja= 0");
+    }
+
+    //LIBT
+    gotoxy(0,5);
+    printf("// Lista Invertida con Busqueda por Triseccion");
+    gotoxy(0,6);
+
+    //COSTOS MAXIMOS
+    printf("// Costos maximos ->");
+    gotoxy(25,6);
+    printf("Evocacion Exitosa= %d", maxevocarELIBT);
+    gotoxy(55,6);
+    printf("Evocacion Fracaso= %d", maxevocarFLIBT);
+    gotoxy(85,6);
+    printf("Alta= %d", maxaltaLIBT);
+    gotoxy(100,6);
+    printf("Baja= %d", maxbajaLIBT);
+    gotoxy(0,7);
+    printf("// Costos Medios  ->");
+    gotoxy(25,7);
+
+    //COSTOS MEDIOS
+    if(cantEvocarELIBT!=0)
+    {
+        printf("Evocacion Exitosa= %.2f", sumaEvocarELIBT/cantEvocarELIBT);
+    }
+    else
+    {
+        printf("Evocacion Exitosa= 0");
+    }
+    gotoxy(55,7);
+    if(cantEvocarFLIBT)
+    {
+        printf("Evocacion Fracaso= %.2f", sumaEvocarFLIBT/cantEvocarFLIBT);
+    }
+    else
+    {
+        printf("Evocacion Fracaso= 0");
+    }
+    gotoxy(85,7);
+    if(cantAltaLIBT!=0)
+    {
+        printf("Alta= %.2f", sumaAltaLIBT/cantAltaLIBT);
+    }
+    else
+    {
+        printf("Alta= 0");
+    }
+    gotoxy(100,7);
+    if(cantBajaLIBT!=0)
+    {
+        printf("Baja= %.2f", sumaBajaLIBT/cantBajaLIBT);
+    }
+    else
+    {
+        printf("Baja= 0");
+    }
+
+    /*gotoxy(0,9);
+    printf("// Arbol binario de busqueda");
+    gotoxy(0,10);
+    printf("// Costos maximos ->");
+    gotoxy(25,10);
+    printf("Evocacion Exitosa= %d", maxEvoABE);
+    gotoxy(55,10);
+    printf("Evocacion Fracaso= %d", maxEvoABF);
+    gotoxy(85,10);
+    printf("Alta= %.2f", maxAltaAB);
+    gotoxy(100,10);
+    printf("Baja= %.2f", maxBajaAB);
+    gotoxy(0,11);
+    printf("// Costos Medios  ->");
+    gotoxy(25,11);
+
+    if(cantEvoEABB!=0) printf("Evocacion Exitosa= %.2f", sumaEvoABE/cantEvoEABB);
+    else printf("Evocacion Exitosa= 0");
+
+    gotoxy(55,11);
+    if(cantEvoFABB!=0) printf("Evocacion Fracaso= %.2f", sumaEvoABF/cantEvoFABB);
+    else printf("Evocacion Fracaso= 0");
+
+    gotoxy(85,11);
+    if(cantAltaABB!=0) printf("Alta= %.2f", sumaAltaAB/cantAltaABB);
+    else printf("Alta= 0");
+
+    gotoxy(100,11);
+    if(cantBajaABB!=0) printf("Baja= %.2f", sumaBajaAB/cantBajaABB);
+    else printf("Baja= 0");*/
+
+    gotoxy(0,13);
+}
+return 1;
 }
 
 void MostraLSO(Alumno lso[],int cant)
@@ -291,7 +392,7 @@ void MostraLSO(Alumno lso[],int cant)
 int main()
 {
     Alumno LSO[TAM];
-    Alumno LIBT*[TAM];
+    Alumno *LIBT[TAM];
     char codigo[8];
     int opcion;
     int cantLSO=0, cantLIBT=0;
@@ -315,7 +416,7 @@ int main()
         switch (opcion)
         {
         case 1:
-            compararEstructuras(LSO,&cantLSO,LIBT,&cantLIBT);
+            compararEstructuras(LSO,LIBT,&cantLSO,&cantLIBT);
             system("pause");
             system("cls");
             break;
